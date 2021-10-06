d9ace69a2ce95c2f5db0dd0a81a9941e3044cfc5
sakaifee <shadanalamkaifee@gmail.com>
2021-10-06 11:24:36 +0530

    button code updated

--- app.js
+++ app.js
@@ -14,5 +14,31 @@ todoButton.addEventListener("click", addTodo);
 function addTodo(event) {
     //Prevent form from submitting 
     event.preventDefault();
-    // console.log("hello");
+    
+
+    //Todo DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //Create LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = "hey";
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo); 
+    //Check Mark
+    const completeButton = document.createElement("button");
+    completeButton.innerHTML = '<i class="fas fa-check"></i>';
+    completeButton.classList.add("complete-btn");
+    todoDiv.appendChild(completeButton);
+
+    //Check trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-button");
+    todoDiv.appendChild(trashButton);
+
+
+    //Append to list
+    todoList.appendChild(todoDiv); 
+
 }
